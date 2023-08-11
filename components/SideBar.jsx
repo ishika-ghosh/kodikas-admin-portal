38f59b70a737360e950535a7940f6e795e2b7485
Ishika Ghosh <idofishika321@gmail.com>
2023-08-12 01:13:35 +0530

    timing updated

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -183,7 +183,7 @@ function SideBar() {
                     </span>
                   </Link>
                 </li>
-                <li>
+                <li onClick={handleClick}>
                   <Link
                     href="/dashboard/event-timings"
                     className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
