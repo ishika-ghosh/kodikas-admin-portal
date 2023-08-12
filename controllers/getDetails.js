7bc5f49aaec87118bc57117afc26587487e0b3f2
Ishika Ghosh <idofishika321@gmail.com>
2023-08-12 19:21:01 +0530

    get details problem

--- controllers/getDetails.js
+++ controllers/getDetails.js
@@ -1,9 +1,9 @@
-import jwt from "jsonwebtoken";
+import verify from "jsonwebtoken/verify";
 export const getDetails = (request) => {
   try {
-    const token = request.cookies.get("token")?.value;
+    const token = request.cookies.get("token")?.value || "";
     console.log(token);
-    const decoded = jwt.verify(token, process.env.MONGO_SECRET, (err, res) => {
+    const decoded = verify(token, process.env.MONGO_SECRET, (err, res) => {
       if (err) {
         console.log(err);
         return null;
